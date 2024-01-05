bbb5f7dd8b908ab7a1b26f404de7a1ca481239f8
Matej Bačo <matejbaco2000@gmail.com>
2024-01-05 19:38:43 +0100

    Improve hardmoed UI

--- src/routes/me/game/+page.svelte
+++ src/routes/me/game/+page.svelte
@@ -168,16 +168,14 @@
 									{#if hasHardmodeStarted && isHardmodeCompleted}
 										<svg
 											xmlns="http://www.w3.org/2000/svg"
-											fill="none"
 											viewBox="0 0 24 24"
-											stroke-width="2"
-											stroke="currentColor"
+											fill="currentColor"
 											class="w-5 h-5 text-purple-600"
 										>
 											<path
-												stroke-linecap="round"
-												stroke-linejoin="round"
-												d="m4.5 12.75 6 6 9-13.5"
+												fill-rule="evenodd"
+												d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.006 5.404.434c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.434 2.082-5.005Z"
+												clip-rule="evenodd"
 											/>
 										</svg>
 									{:else}
